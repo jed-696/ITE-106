cac43d9c9dca4fa9000f3074e619d4fd4f029d7e
jed-696 <jedbusinos@gmail.com>
2024-10-03 16:16:10 +0800

    Delete Bicycle.java

--- Bicycle.java
+++
@@ -1,9 +0,0 @@
-public class Bicycle {
-    private int gear = 5;
-    public int getGear() {
-        return gear;
-    }
-    public void braking() {
-        System.out.println("Braking is working");
-    }
-}
