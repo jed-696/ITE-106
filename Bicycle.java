6858e66c8a47e6b4ab3786fabdb2109b7d87c405
jed-696 <jedbusinos@gmail.com>
2024-10-03 16:12:32 +0800

    Create Bicycle.java

---
+++ Bicycle.java
@@ -0,0 +1,9 @@
+public class Bicycle {
+    private int gear = 5;
+    public int getGear() {
+        return gear;
+    }
+    public void braking() {
+        System.out.println("Braking is working");
+    }
+}
