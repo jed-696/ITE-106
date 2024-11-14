7d1cfcb00ab4f680da6d1eb596c53cba99149f81
jed-696 <jedbusinos@gmail.com>
2024-11-14 15:43:33 +0800

    Create Main.java

---
+++ Bicycle/Main.java
@@ -0,0 +1,9 @@
+public class MainBicycle {
+	public static void main (String[] args) {
+		
+		Bicycle sportsBicycle = new Bicycle();
+		
+		System.out.println(sportsBicycle.gear);
+		sportsBicycle.braking();
+	}
+}
