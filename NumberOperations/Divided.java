378bc1b7c740a1571fb1de1511339767d7c6b56a
jed-696 <jedbusinos@gmail.com>
2024-11-14 15:45:24 +0800

    Create Divided.java

---
+++ NumberOperations/Divided.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Divided
+{
+	public static void main (String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	System.out.println(" ");
+	double num1 = scanner.nextDouble();
+	System.out.println(" ");
+	double num2 = scanner.nextDouble();
+	double result = num1 / num2;
+	System.out.println(result);
+	}
+}
