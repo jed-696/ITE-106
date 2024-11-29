38d823348f0e2fd40c37b97fb7dce1c796a74c53
jed-696 <jedbusinos@gmail.com>
2024-11-29 21:50:41 +0800

    Delete MenuBar directory

--- MenuBar/ACTIVITY_01/MyActionListener.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-
-public class MyActionListener implements ActionListener {
-    public void actionPerformed (ActionEvent e) {
-		Toolkit.getDefaultToolkit().beep();
-    }
-}
